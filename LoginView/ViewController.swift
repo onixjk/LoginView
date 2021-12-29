3937a9c586196beb254f77ee2d80f18d81f0fe7a
onixjk <onixjkk@gmail.ru>
2021-12-29 17:20:23 +0300

    setup view controller

--- LoginView/ViewController.swift
+++ LoginView/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userNameTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    
+    @IBOutlet weak var forgotUserNameButton: UIButton!
+    @IBOutlet weak var forgotPasswordButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func forgotUserNamePressed() {
+        showAlert(with: "Oops!", and: "Your name is User")
+    }
+    
+    @IBAction func forgotPasswordPressed() {
+    }
+    
+    private func showAlert(with title: String, and message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        present(alert, animated: true) //sfklgksfg
+    }
+    
 }
 
