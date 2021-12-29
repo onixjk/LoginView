22f1183d4a613073ef03a27d155ba1b296aa674c
onixjk <onixjkk@gmail.ru>
2021-12-29 20:03:16 +0300

    Welcome view controller done

---
+++ LoginView/WelcomeViewController.swift
@@ -0,0 +1,15 @@
+//
+//  WelcomeViewController.swift
+//  LoginView
+//
+//  Created by Onix qq on 29.12.21.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+    
+    @IBAction func logOutPressed() {
+        dismiss(animated: true)
+    }
+}
