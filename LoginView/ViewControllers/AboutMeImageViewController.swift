e9e42bb32b9f40b79dab82b98d12b310239f3da0
onixjk <onixjkk@gmail.ru>
2022-01-02 20:43:48 +0300

    Create and coding new View Controllers

---
+++ LoginView/ViewControllers/AboutMeImageViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AboutMeImageViewController.swift
+//  LoginView
+//
+//  Created by Onix qq on 2.01.22.
+//
+
+import UIKit
+
+class AboutMeImageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
---
+++ LoginView/ViewControllers/AboutMeViewController.swift
@@ -0,0 +1,23 @@
+//
+//  AboutMeViewController.swift
+//  LoginView
+//
+//  Created by Onix qq on 2.01.22.
+//
+
+import UIKit
+
+class AboutMeViewController: UIViewController {
+    
+    // MARK: - Override methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    // MARK: IB Action
+    @IBAction func unwind(for unwindSegue: UIStoryboardSegue, towards subsequentVC: AboutMeViewController) {
+        
+    }
+
+}
